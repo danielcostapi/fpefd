449c80121e22c68145bad9f3058127980639e5bc
danielcostapi <danielluis2@gmail.com>
2018-02-02 17:33:06 -0200

    02/08/2018 - inicio painel acm

---
+++ acm/configs/config.php
@@ -0,0 +1,8 @@
+<?php
+	
+	/* Configurações Painel Administrativo*/
+	
+	define ('titulo_barra', 'Federacao Pernambucana de Futebol Digital');  //Titulo do Painel.
+	define ('titulo_logo', 'FPEFD');  //Titulo do Painel.
+
+?>
