e46557f9364d9da4339388d0c176a4b1828370e6
danielcostapi <danielluis2@gmail.com>
2018-02-03 20:23:55 -0200

    Layout Inicial

---
+++ header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>:: FPEFD - Federação Pernambucana de Futebol Digital</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+  <!-- Bootstrap -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!--==========================
+    Header
+  ============================-->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">FPEFD</a></h1>
+        <!-- Desconheça abaixo se você preferir usar um logotipo de imagem -->
+        <!-- <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="#intro">Inicio</a></li>
+          <li class="menu-has-children"><a href="">A FPEFD</a>
+            <ul>
+              <li><a href="#">Sobre</a></li>
+              <li><a href="#">Estatuto</a></li>
+              <li><a href="#">Diretoria</a></li>
+            </ul>
+          </li>
+           <li class="menu-has-children"><a href="">Cadastro</a>
+            <ul>
+              <li><a href="cadastro-cyber-atletas.php">Cyber-Atletas</a></li>
+              <li><a href="#">Ligas Municipais</a></li>
+              
+            </ul>
+          </li>
+          <li class="menu-has-children"><a href="">Ranking</a>
+            <ul>
+              <li><a href="#">Geral</a></li>
+              <li><a href="#">Municipal</a></li>
+              <li><a href="#">Critérios de Pontuação</a></li>
+            </ul>
+          </li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#team">Team</a></li>
+          <li class="menu-has-children"><a href="">Drop Down</a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+              <li><a href="#">Drop Down 5</a></li>
+            </ul>
+          </li>
+          <li><a href="#contact">Contato</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- #header -->
+
+  <!--==========================
+    Seção Intro
+  ============================-->
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active" style="background-image: url('img/intro-carousel/1.jpg');">
+            <div class="carousel-container">
+             <!-- --> 
+            </div>
+          </div>
+
+
+        </div>
+        
+      </div>
+    </div>
+  </section><!-- #intro -->
